0791cc6ad6c8aeb87eb181eec32200c591059525
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-07-08 19:52:47 -0400

    Add navLinks

---
+++ components/HostLayout.jsx
@@ -0,0 +1,36 @@
+
+import React from "react"
+import { NavLink, Outlet } from "react-router-dom"
+
+export default function HostLayout() {
+
+    return (
+        <>
+            <nav className="host-nav">
+                <NavLink
+                    to="/host"
+                    end
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Dashboard
+                </NavLink>
+
+                <NavLink
+                    to="/host/income"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Income
+                </NavLink>
+
+                <NavLink
+                    to="/host/reviews"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Reviews
+                </NavLink>
+
+            </nav>
+            <Outlet />
+        </>
+    )
+}
