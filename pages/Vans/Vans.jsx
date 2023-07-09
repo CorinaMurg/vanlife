3d41cd25a3bdef68311a5de687992d754f83ca41
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-07-09 18:51:07 -0400

    Update back to based on filter

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -30,7 +30,16 @@ export default function Vans() {
 
     const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-tile">
-            <Link to={`/vans/${van.id}`}>
+            {/* not needed. we are already in /vans. the id is a string already
+            <Link to={`/vans/${van.id}`}>  */}
+            <Link 
+                to={van.id} 
+                state={{ 
+                    search: `?${searchParams.toString()}`, 
+                    // will use for back to vans
+                    type: typeFilter 
+                }}
+            >
                 <img src={van.imageUrl} />
                 <div className="van-info">
                     <h3>{van.name}</h3>
@@ -41,33 +50,46 @@ export default function Vans() {
         </div>
     ))
     
-    /**
-     * Challenge: change the Links to buttons and use the
-     * setSearchParams function to set the search params
-     * when the buttons are clicked. Keep all the classNames
-     * the same.
-     */
+    function handleFilterChange(key, value) {
+        setSearchParams(prevParams => {
+            if (value === null) {
+                prevParams.delete(key)
+            } else {
+                prevParams.set(key, value)
+            }
+            return prevParams
+        })
+    }
 
     return (
         <div className="van-list-container">
             <h1>Explore our van options</h1>
             <div className="van-list-filter-buttons">
-                <button 
-                    onClick={() => setSearchParams({type: "simple"})}
-                    className="van-type simple"
+                <button
+                    onClick={() => handleFilterChange("type", "simple")}
+                    className={
+                        `van-type simple ${typeFilter === "simple" ? "selected" : ""}`
+                    }
                 >Simple</button>
-                <button 
-                    onClick={() => setSearchParams({type: "luxury"})}
-                    className="van-type luxury"
+                <button
+                    onClick={() => handleFilterChange("type", "luxury")}
+                    className={
+                        `van-type luxury ${typeFilter === "luxury" ? "selected" : ""}`
+                    }
                 >Luxury</button>
-                <button 
-                    onClick={() => setSearchParams({type: "rugged"})}
-                    className="van-type rugged"
+                <button
+                    onClick={() => handleFilterChange("type", "rugged")}
+                    className={
+                        `van-type rugged ${typeFilter === "rugged" ? "selected" : ""}`
+                    }
                 >Rugged</button>
-                <button 
-                    onClick={() => setSearchParams({})}
-                    className="van-type clear-filters"
-                >All</button>
+
+                {typeFilter ? (
+                    <button
+                        onClick={() => handleFilterChange("type", null)}
+                        className="van-type clear-filters"
+                    >All</button>
+                ) : null}
             
             </div>
             <div className="van-list">
